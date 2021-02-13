4ae005ea5e695c906f9d5b7df989a49253bf7fac
sibbernsen.dan@gmail.com <sibbernsen.dan@gmail.com>
2021-02-13 00:43:26 -0600

    -removing tfstate materials.

--- backend.tf
+++ backend.tf
@@ -9,9 +9,5 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name   = "tfresourcegroup"
-    storage_account_name  = "tfresourcegroup"
-    container_name        = "tfstate"
-    key                   = "tfstate"
   }
 }
